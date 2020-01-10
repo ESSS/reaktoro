632845178f32b4740126b4f2fdad394e656bd6bd
Alexandre Barbosa Bruno <abarbosa@esss.co>
2020-01-10 14:29:11 -0300

    add test for cubic-EOS

---
+++ Reaktoro/Thermodynamics/EOS/CuibicEOS-test.py
@@ -0,0 +1,12 @@
+from reaktoro import *
+
+def test_Cubic_EOS_multiple_roots():
+    Temperature = 298.15
+    Pressure = 100000.0
+    b = [3.0,
+         122.01687012,
+         1.0,
+         63.50843506,
+         0.0]
+    
+    return 1==0
