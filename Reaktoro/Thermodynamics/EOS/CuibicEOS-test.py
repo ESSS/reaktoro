6b69aca759f8e40bfbbd556d13194493fe408726
Alexandre Barbosa Bruno <abarbosa@esss.co>
2020-01-13 15:45:19 -0300

    add CubicEOS test

--- Reaktoro/Thermodynamics/EOS/CuibicEOS-test.py
+++ Reaktoro/Thermodynamics/EOS/CuibicEOS-test.py
@@ -1,6 +1,22 @@
+import pytest
+
 from reaktoro import *
 
 def test_Cubic_EOS_multiple_roots():
+    
+    database = Database("supcrt98.xml")
+
+    editor = ChemicalEditor(database)
+    editor.addAqueousPhaseWithElementsOf("H2O Fe(OH)2 Fe(OH)3 NH3")
+    editor.addGaseousPhaseWithElementsOf("NH3")
+    editor.addMineralPhase("Magnetite")
+
+    system = ChemicalSystem(editor)
+    
+    state = ChemicalState(system)
+
+    solver = EquilibriumSolver(system)
+
     Temperature = 298.15
     Pressure = 100000.0
     b = [3.0,
@@ -8,5 +24,8 @@ def test_Cubic_EOS_multiple_roots():
          1.0,
          63.50843506,
          0.0]
+   
+    solver.approximate(state, Temperature, Pressure, b)
     
-    return 1==0
+    state.properties()
+    
