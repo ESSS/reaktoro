4119796b695b807c2a236d1dc88d8552474a8a87
Diego Tavares Volpatto <diego@esss.co>
2022-09-02 20:28:38 -0300

    Add a test to check if the NIST properties are propagated
    (STEPS-174)

--- tests/test_nist_database_usage.py
+++ tests/test_nist_database_usage.py
@@ -1,9 +1,25 @@
+import pytest
 from reaktoro import (
     ChemicalEditor,
     ChemicalSystem,
+    ChemicalProperties,
 )
 
 
-def test_reaktoro_setup_with_nist_database(nist_database):
-    editor = ChemicalEditor(nist_database)
+@pytest.mark.parametrize(
+    "species_name", ["Na+", "Cl-", "H2O(l)"]
+)
+def test_reaktoro_setup_with_nist_database(species_name, nist_database, chemical_editor_nacl_nist):
+    editor = chemical_editor_nacl_nist
+
     system = ChemicalSystem(editor)
+    species_index = system.indexSpecies(species_name)
+
+    properties = ChemicalProperties(system)
+    species_partial_molar_gibbs_energy = properties.standardPartialMolarGibbsEnergies().val
+    species_g0_from_properties = species_partial_molar_gibbs_energy[species_index]
+
+    species_in_database = nist_database.aqueousSpecies(species_name)
+    species_nist_thermodata = species_in_database.thermoData().nist
+    species_g0_from_thermodata = species_nist_thermodata.G0
+    assert species_g0_from_properties == species_g0_from_thermodata
