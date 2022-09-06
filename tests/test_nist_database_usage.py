1ee2b70a1281e5a5c6b161abe83f8706c108412d
Diego Tavares Volpatto <diego@esss.co>
2022-09-06 16:51:45 -0300

    Initialize ChemicalProperties with T and P values in test
    This fixes the test of NIST DB usage as std properties. So, for ChemicalProperties, if the ChemicalState is not initialized with P and T, junk values are used and the properties are not properly calculated
    
    (STEPS-174)

--- tests/test_nist_database_usage.py
+++ tests/test_nist_database_usage.py
@@ -6,7 +6,6 @@ from reaktoro import (
 )
 
 
-@pytest.mark.xfail(reason="Unable to set NIST DB as std properties.")
 @pytest.mark.parametrize(
     "species_name", ["Na+", "Cl-", "H2O(l)"]
 )
@@ -17,13 +16,16 @@ def test_reaktoro_setup_with_nist_database(species_name, nist_database, chemical
     species_index = system.indexSpecies(species_name)
 
     properties = ChemicalProperties(system)
+    P_std = 1e5
+    T_std = 298.15
+    properties.update(T_std, P_std)
     species_partial_molar_gibbs_energy = properties.standardPartialMolarGibbsEnergies().val
     species_g0_from_properties = species_partial_molar_gibbs_energy[species_index]
 
     species_in_database = nist_database.aqueousSpecies(species_name)
     species_nist_thermodata = species_in_database.thermoData().nist
     species_g0_from_thermodata = species_nist_thermodata.G0
-    assert species_g0_from_properties == species_g0_from_thermodata
+    assert species_g0_from_properties == pytest.approx(species_g0_from_thermodata)
 
 
 def test_nist_database_thermodata_type(nist_database):
